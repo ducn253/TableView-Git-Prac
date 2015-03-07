b48d5615f4b693e3eceed40b00006cbeafeb2aa1
Richard Nguyen <Urghh@Richards-MBP.home>
2015-03-07 12:47:38 -0800

    Initial Commit

---
+++ tableViewGitPrac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableViewGitPrac
+//
+//  Created by Richard Nguyen on 3/7/15.
+//  Copyright (c) 2015 Duc Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
